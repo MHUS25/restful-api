cad7efd5a0161636e4c55c536af48f3cdbc7f408
Muna <mmhussein125@gmail.com>
2019-02-03 16:42:19 +0000

    Passes callback function to createServer method

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.write('Hello world!');
+    res.end();
+  }
+});
 
-server.on('connection', (socket) => {
-  console.log('New connection...')
-})
 server.listen(3000);
 
 console.log('Listening on port 3000...')
