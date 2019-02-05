ad4b4620ce53c9ed7602535e31e320ae34ff7543
Muna <mmhussein125@gmail.com>
2019-02-05 01:55:44 +0000

    Refactor using validateCourse function

--- index.js
+++ index.js
@@ -49,11 +49,8 @@ app.put('/api/courses/:id', (req, res) => {
   const course = courses.find( c => c.id === parseInt(req.params.id))
   if (!course) res.status(404).send('The course with the given id was not found.');
 
-  const schema = {
-     name: Joi.string().min(3).required()
-  };
-
-  const result = Joi.validate(req.body, schema);
+  const result = validateCourse(req.body);
+  
   if (result.error) {
     res.status(400).send(result.error.details[0].message);
     return;
@@ -62,6 +59,14 @@ app.put('/api/courses/:id', (req, res) => {
   course.name = req.body.name;
   res.send(course);
 });
+
+function validateCourse(course) {
+  const schema = {
+     name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}`));
