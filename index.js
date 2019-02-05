6e6f89cf06813322cf8e8aa753905e4aec8c9e46
Muna <mmhussein125@gmail.com>
2019-02-05 01:10:28 +0000

    Input validation refactor using joi - object schema validation

--- index.js
+++ index.js
@@ -20,9 +20,14 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-  if (!req.body.name || req.body.name.length < 3) {
-    // 400 Bad request
-    res.status(400).send('Name is reuqired and should have a minimum of three characters.')
+
+  const schema = {
+     name: Joi.string().min(3).required()
+  };
+
+  const result = Joi.validate(req.body, schema);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
     return;
   }
 
