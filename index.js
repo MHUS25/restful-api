9f6f7683584a5a763e3205a47222d285cad4702b
Muna <mmhussein125@gmail.com>
2019-02-05 02:22:38 +0000

    Refactors code blocks to one line

--- index.js
+++ index.js
@@ -22,10 +22,7 @@ app.get('/api/courses', (req, res) => {
 app.post('/api/courses', (req, res) => {
 
   const { error } = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   const course = {
     id: courses.length + 1,
@@ -43,16 +40,10 @@ app.get('/api/courses/:id', (req, res) => {
 
 app.put('/api/courses/:id', (req, res) => {
   const course = courses.find( c => c.id === parseInt(req.params.id))
-  if (!course) {
-    res.status(404).send('The course with the given id was not found.');
-    return;
-  }
+  if (!course) return res.status(404).send('The course with the given id was not found.');
 
   const { error } = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   course.name = req.body.name;
   res.send(course);
@@ -60,10 +51,7 @@ app.put('/api/courses/:id', (req, res) => {
 
 app.delete('/api/courses/:id', (req, res) => {
   const course = courses.find( c => c.id === parseInt(req.params.id))
-  if (!course) {
-    res.status(404).send('The course with the given id was not found.');
-    return;
-  }
+  if (!course) return res.status(404).send('The course with the given id was not found.');
 
   const index = courses.indexOf(course);
   courses.splice(index, 1);
